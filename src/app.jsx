2c5fc13eb5b4e588424b90a9af820af17992df27
Anna <ajwleach@gmail.com>
2017-01-16 08:43:04 +0000

    lift state to parent :arrow_up:

--- src/app.jsx
+++ src/app.jsx
@@ -7,9 +7,9 @@ var thingsInGrid = [];
 
 function CrispPacket(props){
 	return(
-			<div className="item crisps" onClick ={props.onPickUp}>
-				<img src="img/crisps.jpeg" />
-			</div>
+        <div className="item crisps" onClick ={props.onPickUp}>
+            <img src="img/crisps.jpeg" />
+        </div>
 		)
 };
 CrispPacket.propTypes = {
@@ -18,80 +18,155 @@ CrispPacket.propTypes = {
 
 function Leaf(props){
     return(
-		<div className="item leaf" onClick={props.onLeafPick}>
-				<img src="img/yellow-leaf.png" />
+		<div className="item leaf" onClick={props.onPickUp}>
+            <img src="img/yellow-leaf.png" />
 		</div>
     )
 };
 Leaf.propTypes = {
-	onLeafPick: React.PropTypes.func.isRequired
+	onPickUp: React.PropTypes.func.isRequired
 };
 
 function BinBag(props){
-	return(
-			<div className="binbag">
-				<div>
-					<span className="big-number">{props.score}</span>
-				</div>
-			</div>
-		)
+    return(
+        <div className="binbag">
+            <div>
+                <span className="big-number">{props.score}</span>
+            </div>
+        </div>
+    )
 };
 BinBag.propTypes = {
 	score: React.PropTypes.number.isRequired
+};
+
+
+class StopWatch extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(this.props.onTick, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.props.interval);
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <p>{this.props.elapsed}</p>
+                { this.props.running ?
+                    <button disabled="true">Go go go!</button>
+                    :
+                    <button onClick={this.props.onStart }>Start the game!</button>
+                }
+            </div>
+        )
+    }
+};
+StopWatch.propTypes = {
+    running: React.PropTypes.bool.isRequired,
+    elapsed: React.PropTypes.number.isRequired,
+    onStart: React.PropTypes.func.isRequired
+};
+
+class GameGrid extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        setInterval(this.indexOfTrash, 1000);
+    }
+
+    render(){
+        let thingsInGrid = [];
+        for(let i=0; i < NUMBERITEMS; i++) {
+            thingsInGrid.push( <Leaf key={i.toString()} onPickUp={ this.props.onPickUp }/> );
+        }
+        thingsInGrid.map((item, index) => {
+            if (index === this.props.trash) {
+                thingsInGrid.splice( index, 1, <CrispPacket key={index.toString()} onPickUp={ this.props.onPickUp }/> );
+            }
+        });
+
+        return (
+           <div className="grid">
+               {thingsInGrid}
+           </div>
+        )
+    }
+}
+GameGrid.propTypes = {
+    trash: React.PropTypes.number.isRequired,
+    onPickUp: React.PropTypes.func.isRequired,
+    running: React.PropTypes.bool.isRequired
 };
 
 class App extends React.Component {
 
    constructor(props){
        super(props);
-       this.state = { score: 0, trash: 0 };
+       this.state = { score: 0, trash: 0, running: false, elapsed: 0 };
+
        this.onPickUp = this.onPickUp.bind(this);
-       this.onLeafPick = this.onLeafPick.bind(this);
-       this.trashNumber = this.trashNumber.bind(this);
+       this.indexOfTrash = this.indexOfTrash.bind(this);
+       this.onStart = this.onStart.bind(this);
+       this.onTick = this.onTick.bind(this);
+       this.onStop = this.onStop.bind(this);
     }
 
-    onPickUp() {
-        this.state.score += 1;
-        this.setState({score: this.state.score});
-        this.trashNumber();
+    onStart(){
+        console.log("this click thing set running");
+        this.setState({running: true});
     }
 
-    onLeafPick() {
-        this.state.score -= 1;
-        this.setState({score: this.state.score});
-        this.trashNumber();
+    onTick(){
+        if(this.state.running == true){
+            if(this.state.elapsed < 30){
+                this.state.elapsed += 1;
+                this.setState({ elapsed: this.state.elapsed });
+            }
+            else{
+                this.onStop();
+            }
+        }
     }
 
-    trashNumber(){
+    onStop(){
+        this.setState({
+            running: false,
+            elapsed: 0
+        });
+    }
+
+    indexOfTrash(){
         let rando = Math.floor(Math.random() * NUMBERITEMS );
         this.setState({ trash: rando });
     }
 
-    componentDidMount(){
-        setInterval(this.trashNumber, 1000);
+    onPickUp(e) {
+        if(e.target.parentNode.classList.contains("crisps")){
+            this.state.score += 1;
+        }
+        else{
+            this.state.score -= 1;
+        }
+        this.setState({score: this.state.score});
+        this.indexOfTrash();
     }
 
-
     render() {
-        let thingsInGrid = [];
-
-        for(let i=0; i < NUMBERITEMS; i++) {
-            thingsInGrid.push( <Leaf key={i.toString()} onLeafPick={ this.onLeafPick }/> );
-        }
-
-        thingsInGrid.map((item, index) => {
-            if (index === this.state.trash) {
-                thingsInGrid.splice( index, 1, <CrispPacket key={index.toString()} onPickUp={ this.onPickUp }/> );
-            }
-        });
-
         return (
 			<div className="App">
 				<BinBag score={this.state.score}/>
 				<p>Hello {this.props.name} can you complete this task?</p>
-				<div className="grid">
-                    { thingsInGrid }
-				</div>
+                <StopWatch elapsed={this.state.elapsed} onClick={this.onStart} running={this.state.running}/>
+                <GameGrid running={this.state.running} trash={this.state.trash} onPickUp={this.onPickUp}/>
 			</div>
         )
     }
@@ -102,4 +177,4 @@ App.propTypes = {
     score: React.PropTypes.number
 };
 
-ReactDOM.render(<App name="challenger" score={0}/>, document.getElementById("root"));
+ReactDOM.render(<App name="challenger" />, document.getElementById("root"));
